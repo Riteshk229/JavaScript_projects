7efda5f73ab23af624b08288953c1433ef273141
Ritesh <Riteshk229@gmail.com>
2023-06-26 17:49:13 +0530

    used toggle

--- navbar toggle/index.js
+++ navbar toggle/index.js
@@ -6,9 +6,12 @@ toggle.addEventListener('click',function(){
 
     // .contains in .classList return boolean value 
     // console.log(links.classList.contains('links'));
-    if(links.classList.contains('show-links')){
-        links.classList.remove('show-links');
-    } else{
-        links.classList.add("show-links");
-    }
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // } else{
+    //     links.classList.add("show-links");
+    // }
+
+    // lines 9 to 13 could also be written as 
+    links.classList.toggle('show-links');
 });

b839310779a9a6669e99bddeb21d16db4291ade1
Ritesh <Riteshk229@gmail.com>
2023-06-26 17:38:19 +0530

    Created Navbar Toggler

---
+++ navbar toggle/index.js
@@ -0,0 +1,14 @@
+const toggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+toggle.addEventListener('click',function(){
+    console.log(links);
+
+    // .contains in .classList return boolean value 
+    // console.log(links.classList.contains('links'));
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links');
+    } else{
+        links.classList.add("show-links");
+    }
+});

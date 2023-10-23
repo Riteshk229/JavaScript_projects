496058847c3ee2cfd1769193b6f28953222d8bba
Ritesh <Riteshk229@gmail.com>
2023-10-23 12:23:54 +0530

    smooth scroll for  smaller screen

--- Scroll/index.js
+++ Scroll/index.js
@@ -61,8 +61,19 @@ scrollLinks.forEach(function (link) {
         // navigate to speccific  spot
         const id = e.currentTarget.getAttribute('href').slice(1);
         const element = document.getElementById(id);
-        let position = element.offsetTop;
-        // console.log(position);
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+        let position = element.offsetTop -navHeight;
+        console.log(position);
+
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
         window.scrollTo({
             left: 0,
             top: position
